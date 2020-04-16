99d132605c4d3aea788d628e64c72fc6ff1e3d76
Rachael Andrew <r.andrew.dev@gmail.com>
2020-04-15 22:25:32 -0500

    moved images folder to src, added neat function to import all the images, found on stack overflow, details in comments

---
+++ click-on-elmo/src/components/Footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return <div className="navbar fixed-bottom"></div>;
+}
+
+export default Container;
--- click-on-elmo/src/components/FriendCard/index.js
+++ click-on-elmo/src/components/FriendCard/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./style.css";
 
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+const images = importAll(require.context('../../images', false, /\.(png|jpe?g|svg)$/));
+
+// Credit to https://stackoverflow.com/questions/42118296/dynamically-import-images-from-a-directory-using-webpack
+// for this great function to import all my images and Kathy TA for finding it! 
+
 function FriendCard(props) {
+  console.log(props.image)
   return (
     <div className="click-image">
-        <img alt={props.name} src={props.image} />
+        <img alt={props.name} src={images[props.image]}/>
       </div>
   )}
 
