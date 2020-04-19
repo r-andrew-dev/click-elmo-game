1ac0df1678699307ae5dbfdf3f6db66a134a9412
Rachael Andrew <r.andrew.dev@gmail.com>
2020-04-18 21:20:32 -0500

    Updates

---
+++ precache-manifest.b592a3562fea0bca2561176319ab270f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64590fd1ec9ffab3a57c7d140b73e089",
+    "url": "/click-elmo-game/index.html"
+  },
+  {
+    "revision": "168c6eccba1221940d06",
+    "url": "/click-elmo-game/static/css/main.ec653367.chunk.css"
+  },
+  {
+    "revision": "28a2fd83087cabcfadd2",
+    "url": "/click-elmo-game/static/js/2.f2984fa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/click-elmo-game/static/js/2.f2984fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "168c6eccba1221940d06",
+    "url": "/click-elmo-game/static/js/main.f849aba4.chunk.js"
+  },
+  {
+    "revision": "43bb82a230b3e480e102",
+    "url": "/click-elmo-game/static/js/runtime-main.df5396a0.js"
+  },
+  {
+    "revision": "e387d51e105675bf8bbdb6f90fcc24e9",
+    "url": "/click-elmo-game/static/media/ABCbackground.e387d51e.jpg"
+  },
+  {
+    "revision": "7ed41b4b7026880b765838f94d1e4232",
+    "url": "/click-elmo-game/static/media/Elmo1.7ed41b4b.png"
+  },
+  {
+    "revision": "7def0b0bcec6874f0cfc22b3d20a4717",
+    "url": "/click-elmo-game/static/media/Elmo10.7def0b0b.png"
+  },
+  {
+    "revision": "6ed77b9606f54f3394e3b76644abe769",
+    "url": "/click-elmo-game/static/media/Elmo11.6ed77b96.png"
+  },
+  {
+    "revision": "91b8ec6b47729d9079a6dab0723a0adc",
+    "url": "/click-elmo-game/static/media/Elmo12.91b8ec6b.png"
+  },
+  {
+    "revision": "c74a1371d6c85d1649042f598039847c",
+    "url": "/click-elmo-game/static/media/Elmo2.c74a1371.png"
+  },
+  {
+    "revision": "d8d9d00687d1daf326eed84ea7d980b3",
+    "url": "/click-elmo-game/static/media/Elmo3.d8d9d006.png"
+  },
+  {
+    "revision": "0ad1ec30a23214b37f0f12181df95c7f",
+    "url": "/click-elmo-game/static/media/Elmo4.0ad1ec30.png"
+  },
+  {
+    "revision": "7240c7c2068280f62e57e3034369cfeb",
+    "url": "/click-elmo-game/static/media/Elmo5.7240c7c2.png"
+  },
+  {
+    "revision": "d53cae0e4d999223e752e1d1236800b0",
+    "url": "/click-elmo-game/static/media/Elmo6.d53cae0e.png"
+  },
+  {
+    "revision": "42f8f3394fc000a55a77b4d6804a6f85",
+    "url": "/click-elmo-game/static/media/Elmo7.42f8f339.png"
+  },
+  {
+    "revision": "e3f35a907dd145efe99b8f7b6605564a",
+    "url": "/click-elmo-game/static/media/Elmo8.e3f35a90.png"
+  },
+  {
+    "revision": "01e64d94468bda02cebf3b7be7f29d88",
+    "url": "/click-elmo-game/static/media/Elmo9.01e64d94.png"
+  },
+  {
+    "revision": "6ed664969602f4e4e3ce91f0bbfd4dd9",
+    "url": "/click-elmo-game/static/media/background.6ed66496.jpg"
+  }
+]);
